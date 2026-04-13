95fe24d54884e1c0900736ac90f904ad7a36012e
畠中 太朗 <k024g0045@g.neec.ac.jp>
2026-04-13 12:47:44 +0900

    Menu項目の追加

--- level_editor.py
+++ level_editor.py
@@ -6,8 +6,8 @@ bl_info = {
     "author": "Taro Kamata",
     "version": (1, 0),
     "blender": (3, 3, 1),
-    "location": "",
-    "description": "レベルエディタ",
+    "location": "トップバー",
+    "description": "トップバーにマニュアル項目を追加するレベルエディタ",
     "warning": "",
     "support": "TESTING",
     "wiki_url": "",
@@ -16,11 +16,35 @@ bl_info = {
 }
 
 
+# マニュアルを開くオペレータ
+class WM_OT_open_manual(bpy.types.Operator):
+    bl_idname = "wm.open_level_editor_manual"
+    bl_label = "レベルエディタのマニュアルを開く"
+    bl_description = "レベルエディタのマニュアルページをブラウザで開きます"
+
+    def execute(self, context):
+        bpy.ops.wm.url_open(url="https://www.blender.org/manual/")
+        return {"FINISHED"}
+
+
+# トップバーにメニュー項目を描画する関数
+def draw_menu_manual(self, context):
+    self.layout.operator(WM_OT_open_manual.bl_idname, text="マニュアル", icon="HELP")
+
+
 # アドオン有効化時コールバック
 def register():
+    bpy.utils.register_class(WM_OT_open_manual)
+    bpy.types.TOPBAR_MT_editor_menus.append(draw_menu_manual)
     print("レベルエディタが有効化されました。")
 
 
 # アドオン無効化時コールバック
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(draw_menu_manual)
+    bpy.utils.unregister_class(WM_OT_open_manual)
     print("レベルエディタが無効化されました。")
+
+
+if __name__ == "__main__":
+    register()
