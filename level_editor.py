1a66bf12f039066f15225483b6a1c09e885055c3
畠中 太朗 <k024g0045@g.neec.ac.jp>
2026-04-13 12:56:09 +0900

    サブメニューの追加

--- level_editor.py
+++ level_editor.py
@@ -1,48 +1,67 @@
 import bpy
 
-# ブレンダーに登録するアドオン情報
+# アドオン情報
 bl_info = {
     "name": "レベルエディタ",
     "author": "Taro Kamata",
     "version": (1, 0),
     "blender": (3, 3, 1),
     "location": "トップバー",
-    "description": "トップバーにマニュアル項目を追加するレベルエディタ",
-    "warning": "",
-    "support": "TESTING",
-    "wiki_url": "",
-    "tracker_url": "",
+    "description": "レベルエディタ",
     "category": "Object",
 }
 
 
-# マニュアルを開くオペレータ
-class WM_OT_open_manual(bpy.types.Operator):
-    bl_idname = "wm.open_level_editor_manual"
-    bl_label = "レベルエディタのマニュアルを開く"
-    bl_description = "レベルエディタのマニュアルページをブラウザで開きます"
+# =========================
+# サブメニュークラス
+# =========================
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
 
-    def execute(self, context):
-        bpy.ops.wm.url_open(url="https://www.blender.org/manual/")
-        return {"FINISHED"}
+    def draw(self, context):
+        layout = self.layout
+
+        # サブメニューの中身
+        layout.operator("wm.url_open", text="マニュアル", icon="HELP").url = (
+            "https://www.blender.org/manual/"
+        )
 
 
-# トップバーにメニュー項目を描画する関数
-def draw_menu_manual(self, context):
-    self.layout.operator(WM_OT_open_manual.bl_idname, text="マニュアル", icon="HELP")
+# =========================
+# トップバーに追加する関数
+# =========================
+def submenu(self, context):
+    self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 
-# アドオン有効化時コールバック
+# =========================
+# 登録するクラスリスト
+# =========================
+classes = (TOPBAR_MT_my_menu,)
+
+
+# =========================
+# 有効化
+# =========================
 def register():
-    bpy.utils.register_class(WM_OT_open_manual)
-    bpy.types.TOPBAR_MT_editor_menus.append(draw_menu_manual)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(submenu)
     print("レベルエディタが有効化されました。")
 
 
-# アドオン無効化時コールバック
+# =========================
+# 無効化
+# =========================
 def unregister():
-    bpy.types.TOPBAR_MT_editor_menus.remove(draw_menu_manual)
-    bpy.utils.unregister_class(WM_OT_open_manual)
+    bpy.types.TOPBAR_MT_editor_menus.remove(submenu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
     print("レベルエディタが無効化されました。")
 
 
